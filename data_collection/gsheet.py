859e31da00c8e9b6c9b8cfa680215c7cd8ef07b0
nebbles <bsg115@ic.ac.uk>
2018-12-23 19:16:48 +0000

    modify run script into new gsheet module

---
+++ data_collection/gsheet.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+
+
+def add_to_sheet(sheet_name, row):
+
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        'data_collection/google_credentials.json', scope)
+
+    gc = gspread.authorize(credentials)
+    doc = gc.open_by_key("1LZsKVQDmauzADwlaHXeJy4nndVv-vmaXoD8lfbPoqQo")
+    doc.worksheet(sheet_name).append_row(row)
+
+
+if __name__ == "__main__":
+    import time
+
+    # TEST BEHAVIOUR TO CHECK IT IS FUNCTIONING PROPERLY
+    for i in range(10):
+        cur_time = time.strftime('%Y-%m-%d %H:%M:%S')
+        row = [cur_time, i]
+        print("Adding to sheet: {}".format(row))
+        add_to_sheet('TEST', row)
+        time.sleep(1)
