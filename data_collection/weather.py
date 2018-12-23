0eb0089bc557349b4189b81ab148b75fac6cd88f
nebbles <bsg115@ic.ac.uk>
2018-12-23 19:28:04 +0000

    update to complete weather collection script

--- data_collection/weather.py
+++ data_collection/weather.py
@@ -1,20 +1,50 @@
+#!/usr/local/bin/python3
 from requests import get
 import json
 from pprint import pprint
+import gsheet
+import time
+import csv
+import sys
+import os
 
 
-def call_dark_sky(key):
-    # dbde = ("51.497999", "-0.174511") # Dyson Building
-    london = ("51.506321", "-0.12714") # Lat, Long for London
-    dsky = "https://api.darksky.net/forecast/{}/{loc[0]:},{loc[1]:}?exclude=minutely,hourly,daily&units=si".format(key, loc=london)
-    resp = get(dsky)
+def get_darksky_weather(key, location):
+    api_url = "https://api.darksky.net/forecast/{}/{loc[0]:},{loc[1]:}?exclude=minutely,hourly,daily&units=si".format(
+        key, loc=location)
+    resp = get(api_url)
     return json.loads(resp.text)
 
 
 if __name__ == "__main__":
-
-    with open('data_collection/credentials.json') as key_file:
+    # Collect API credentials from external JSON file
+    with open(os.path.join(sys.path[0], "credentials.json")) as key_file:
         creds = json.load(key_file)
-    
-    data = call_dark_sky(creds['dark_sky'])
-    pprint(data)
+
+    # Get the current time on server
+    cur_time = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    dbde = ("51.497999", "-0.174511")  # Lat, Long Dyson Building
+    london = ("51.506321", "-0.12714")  # Lat, Long for London
+
+    # Get current weather data for location
+    data = get_darksky_weather(creds['dark_sky'], dbde)
+
+    data_time = data['currently']['time']  # Extract time from API call
+    ds_time = time.strftime('%Y-%m-%d %H:%M:%S',
+                            time.gmtime(int(data_time)))  # Format time to ISO
+
+    ds_temp = data['currently']['apparentTemperature']  # Extract temperature
+    ds_precip = data['currently']['precipProbability']  # Extract precipitation
+
+    # Compile data into new spreadsheet row
+    row = [cur_time, ds_time, ds_temp, ds_precip]
+    # row = [cur_time,cur_time,0,0] # FOR DEBUGGING PURPOSES
+
+    # Appending row to a CSV (as backup to Google Sheet)
+    with open(os.path.join(sys.path[0], "weather.csv"), 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(row)
+
+    # Append new row to end of correct tab in Google Sheet
+    gsheet.add_to_sheet('API-WEATHER', row)
