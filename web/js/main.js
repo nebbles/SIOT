c7801b29fe81548b73b1758f07a516a088c94f8a
nebbles <bsg115@ic.ac.uk>
2019-01-10 15:20:28 +0000

    Update to webpage to include animation - currently disabled

--- web/js/main.js
+++ web/js/main.js
@@ -55,7 +55,10 @@ function temperatureGraph(target, data) {
             text: 'Temperature (°C) over time'
         },
         xAxis: {
-            type: 'datetime'
+            type: 'datetime',
+            tickInterval: 1 * 24 * 3600 * 1000,
+            tickWidth: 1,
+            gridLineWidth: 1
         },
         yAxis: {
             title: {
@@ -66,6 +69,7 @@ function temperatureGraph(target, data) {
             enabled: false
         },
         plotOptions: {
+            series:{color:Highcharts.getOptions().colors[5]},
             area: {
                 fillColor: {
                     linearGradient: {
@@ -75,8 +79,8 @@ function temperatureGraph(target, data) {
                         y2: 1
                     },
                     stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        [0, Highcharts.getOptions().colors[5]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[5]).setOpacity(0).get('rgba')]
                     ]
                 },
                 marker: {
@@ -94,7 +98,7 @@ function temperatureGraph(target, data) {
 
         series: [{
             type: 'area',
-            name: 'USD to EUR',
+            name: 'Temperature',
             data: data
         }]
     });
@@ -115,7 +119,10 @@ function humidityGraph(target, data) {
             text: 'Humidity (%) over time'
         },
         xAxis: {
-            type: 'datetime'
+            type: 'datetime',
+            tickInterval: 1 * 24 * 3600 * 1000,
+            tickWidth: 1,
+            gridLineWidth: 1
         },
         yAxis: {
             title: {
@@ -175,7 +182,10 @@ function pressureGraph(target, data) {
             text: 'Pressure (hPa) over time'
         },
         xAxis: {
-            type: 'datetime'
+            type: 'datetime',
+            tickInterval: 1 * 24 * 3600 * 1000,
+            tickWidth: 1,
+            gridLineWidth: 1
         },
         yAxis: {
             title: {
@@ -186,6 +196,9 @@ function pressureGraph(target, data) {
             enabled: false
         },
         plotOptions: {
+            series: {
+                color: Highcharts.getOptions().colors[2]
+            },
             area: {
                 fillColor: {
                     linearGradient: {
@@ -195,8 +208,8 @@ function pressureGraph(target, data) {
                         y2: 1
                     },
                     stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        [0, Highcharts.getOptions().colors[2]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[2]).setOpacity(0).get('rgba')]
                     ]
                 },
                 marker: {
@@ -235,7 +248,10 @@ function forexGraph(target, data) {
             text: 'GBP to USD exchange rate over time'
         },
         xAxis: {
-            type: 'datetime'
+            type: 'datetime',
+            tickInterval: 1 * 24 * 3600 * 1000,
+            tickWidth: 1,
+            gridLineWidth: 1
         },
         yAxis: {
             title: {
@@ -246,6 +262,9 @@ function forexGraph(target, data) {
             enabled: false
         },
         plotOptions: {
+            series: {
+                color: Highcharts.getOptions().colors[1]
+            },
             area: {
                 fillColor: {
                     linearGradient: {
@@ -255,8 +274,8 @@ function forexGraph(target, data) {
                         y2: 1
                     },
                     stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        [0, Highcharts.getOptions().colors[1]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[1]).setOpacity(0).get('rgba')]
                     ]
                 },
                 marker: {
@@ -299,7 +318,10 @@ function normTrendsGraph(data) {
             text: 'Normalised Data Trends over Time'
         },
         xAxis: {
-            type: 'datetime'
+            type: 'datetime',
+            tickInterval: 1 * 24 * 3600 * 1000,
+            tickWidth: 1,
+            gridLineWidth: 0
         },
         yAxis: {
             title: {
@@ -346,11 +368,19 @@ function correlationGraph(data) {
             text: ''
         },
         subtitle: {
-            text: 'Weather series correlated with Forex at increasing lags'
+            text: 'Weather series correlated with Exchange Rate at increasing lags'
         },
         xAxis: {
             title: {
-                text: 'Lag index (in periods of 10 minutes)'
+                text: 'Lag of Exchange from Weather (in hours)'
+            },
+            tickInterval: 6,
+            tickWidth: 1,
+            gridLineWidth: 1,
+            labels: {
+                formatter: function () {
+                    return this.value/6;
+                }
             }
         },
         yAxis: {
@@ -363,21 +393,21 @@ function correlationGraph(data) {
         },
 
         series: [{
-            type: 'line',
-            name: 'Temperature',
-            data: temp
-        },
-        {
-            type: 'line',
-            name: 'Humidity',
-            data: humi
-        },
-        {
-            type: 'line',
-            name: 'Pressure',
-            data: pres
-        }
-    ]
+                type: 'line',
+                name: 'Temperature',
+                data: temp
+            },
+            {
+                type: 'line',
+                name: 'Humidity',
+                data: humi
+            },
+            {
+                type: 'line',
+                name: 'Pressure',
+                data: pres
+            }
+        ]
     });
 }
 
@@ -394,9 +424,32 @@ function createBasicGraphs(data) {
     forexGraph('forex', forx)
 }
 
+function showPosition(position) {
+    lat = position.coords.latitude.toFixed(5);
+    lon = position.coords.longitude.toFixed(5);
+    x.innerHTML = "Location: " + lat + ", " + lon;
+    document.getElementById('ani-control').setAttribute("style", "background-color: #efefef;");
+}
+
+function getLocation() {
+    x = document.getElementById('geo')
+    if (navigator.geolocation) {
+        x.innerHTML = 'Trying to fetch your current location...'
+        loc = navigator.geolocation.getCurrentPosition(showPosition, function() {
+            x.innerHTML = "You've declined location sharing. This is needed for localised animations."
+            x.setAttribute("style", "text-align: left; font-size: 8pt;")
+            document.getElementById('ani-control').setAttribute("style", "background-color: transparent; padding: 0;");
+        });
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
 document.getElementById('chart-tip').innerHTML = document.ontouchstart === undefined ?
     'Tip: Click and drag in the plot area to zoom in!' : 'Tip: Pinch the chart to zoom in!'
 
 parseData("./data/data_all.csv", createBasicGraphs);
 parseData("./data/data_trends_norm.csv", normTrendsGraph);
 parseData("./data/data_correlations.csv", correlationGraph);
+
+getLocation()
---
+++ web/js/rain.js
@@ -0,0 +1,68 @@
+// function makeRequest() {
+//     url = "https://api.darksky.net/forecast/{apikey}/{loc[0]:},{loc[1]:}?exclude=minutely,hourly,daily&units=si"
+//     var request = new XMLHttpRequest();
+//     request.open('GET', url, true);
+//     request.onload = function () {
+//         var data = JSON.parse(this.response);
+//         if (request.status >= 200 && request.status < 400) {
+//             console.log(data)
+//         } else {
+//             console.log('There was an error with the API request.');
+//         }
+//     }
+//     request.send();
+// }
+
+class Drop {
+    constructor() {
+        this.x = random(width);
+        this.y = random(-height, 0);
+        this.z = random(0, 20);
+        this.len = map(this.z, 0, 20, 10, 20);
+        this.yspeed = map(this.z, 0, 20, 4, 10);
+    }
+
+    fall() {
+        this.y = this.y + this.yspeed;
+        this.yspeed = this.yspeed + 0.05;
+
+        if (this.y > height) {
+            this.y = random(-200, -100);
+            this.yspeed = map(this.z, 0, 20, 4, 10);
+        }
+    }
+
+    show() {
+        stroke(138, 43, 226, 50);
+        line(this.x, this.y, this.x, this.y+10)
+    }
+}
+
+var drops;
+var toggle;
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.style('z-index', '-1');
+
+    drops = [];
+    for (let i=0; i < 500; i++) {
+        drops.push(new Drop());
+    }
+    toggle = false; // TODO
+}
+
+function draw() {
+    if (toggle) {
+        background(255);
+        for (drop of drops) {
+            drop.fall()
+            drop.show()
+        }
+    }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
